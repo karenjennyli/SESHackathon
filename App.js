b2fa997d1eeae20b76459418ebfad6530e8b8a69
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2021-05-27 21:43:30 -0500

    added navigation features (BROKEN)

--- App.js
+++ App.js
@@ -2,13 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import ShoppingListPage from "./src/Pages/ShoppingListPage";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <ShoppingListPage />
-        </View>
+        // <View style={styles.container}>
+        //     <ShoppingListPage />
+        // </View>
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="ShoppingListPage">
+                <Drawer.Screen
+                    name="ShoppingListPage"
+                    component={ShoppingListPage}
+                />
+                <Drawer.Screen name="Scanner" component={Scanner} />
+            </Drawer.Navigator>
+        </NavigationContainer>
     );
 }
 
